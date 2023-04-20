0b2dea49839ff6c75eadb5e391893124f665cf03
AdityaNair07 <nairaditya07@gmail.com>
2023-04-20 12:06:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+import weather1 from "./images/weather1.png";
 
 function App() {
+  const apiKey = "196b70161aa143d2b34153020231904 ";
+  const [data, setdata] = useState({});
+  const [inputCity, setinputCity] = useState("");
+  const getWeatherDetails = (city) => {
+    if (!city) return;
+    const apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=1&aqi=no&alerts=no`;
+    axios
+      .get(apiURL)
+      .then((res) => {
+        setdata(res.data);
+      })
+      .catch((e) => {
+        console.log("Error: ", e);
+      });
+  };
+  const handleChange = (e) => {
+    setinputCity(e.target.value);
+  };
+  const handleSearch = () => {
+    getWeatherDetails(inputCity);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>Weather Application</h1>
+        <img
+          src={weather1}
+          alt="WeatherIcon"
+          style={{ width: "100px", padding: "10px" }}
+        />
+      </div>
+      <div className="main">
+        <input
+          type="text"
+          className="searchbar"
+          onChange={handleChange}
+          value={inputCity}
+        />
+        <button className="searchBtn" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+      {Object.keys(data).length > 0 && (
+        <div className="weatherResult">
+          <table cellSpacing={10}>
+            <tr>
+              <td>City:</td>
+              <td>{data.location.name}</td>
+            </tr>
+            <tr>
+              <td>Day: </td>
+              <td>{data.current.condition.text}</td>
+            </tr>
+            <tr>
+              <td>Max: </td>
+              <td>{data.forecast.forecastday[0].day.maxtemp_c}°C</td>
+            </tr>
+            <tr>
+              <td>Min: </td>
+              <td>{data.forecast.forecastday[0].day.mintemp_c}°C</td>
+            </tr>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
